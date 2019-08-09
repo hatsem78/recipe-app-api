b4e87fb886ee0e0a05d0cecbb4c6c31304077b03
Hatsembiller Octavio <hatsem78@gmail.com>
2019-08-09 19:00:42 -0300

    Finaly model user

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelsTests(TestCase):
 
-    def  test_create_user_with_email_successful(self):
+    def test_create_user_with_email_successful(self):
         """Test creating a new user with email successful"""
         email = "test@gmail.com"
         password = "Test@1234"
@@ -22,9 +23,19 @@ class ModelsTests(TestCase):
         user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
+
     def test_new_user_invalid_email(self):
         """Test creating user with not email raises error"""
 
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
 
+    def test_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@londonappdev.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
